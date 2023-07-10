932939d3a9d94c69c9921c687bef8e1a13dd1c79
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-07-10 20:33:54 +0300

    Seed Database With Needed Roles

---
+++ API with JWT Authentication/Migrations/20230710172239_SeadRples.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_with_JWT_Authentication.Migrations
+{
+    public partial class SeadRples : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
